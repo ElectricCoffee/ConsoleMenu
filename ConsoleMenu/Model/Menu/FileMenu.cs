442927d5fdd9eb26f2517fde0316ad24f89c1235
Niko Lepka <slench102+git@gmail.com>
2015-03-19 12:47:54 +0100

    DONE

--- ConsoleMenu/Model/Menu/FileMenu.cs
+++ ConsoleMenu/Model/Menu/FileMenu.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,18 @@ namespace ConsoleMenu.Model.Menu
 {
     class FileMenu : MenuItem
     {
-        //public String 
         public override string DisplayContents()
         {
-            return Contents;
+            var file = new FileInfo(Source);
+
+            return String.Format(
+                "Name         : {0}\n" +
+                "Size         : {1}B\n"+
+                "Attributes   : {2}\n" +
+                "Read-Only    : {3}\n" +
+                "Last Accessed: {4}\n" +
+                "Last Written : {5}",
+                Source, file.Length, file.Attributes.ToString(), file.IsReadOnly, file.LastAccessTime, file.LastWriteTime);
         }
     }
 }
--- ConsoleMenu/Model/Menu/FolderMenu.cs
+++ ConsoleMenu/Model/Menu/FolderMenu.cs
@@ -9,20 +9,28 @@ namespace ConsoleMenu.Model.Menu
 {
     class FolderMenu : MenuItem
     {
-
-
         public override string DisplayContents()
         {
             if (Directory.Exists(Source))
             {
-                var dir = new DirectoryInfo(Source);
+                var directory = new DirectoryInfo(Source);
                 Submenus = new List<MenuItem>();
-                foreach (var file in dir.GetFiles())
+                foreach (var file in directory.GetFiles())
                 {
                     var menuItem = new FileMenu();
-                    menuItem.Title = file.Name;
+                    menuItem.Title = "[F] " + file.Name;
                     menuItem.Source = file.FullName;
                     menuItem.SourceType = Model.SourceType.File;
+
+                    Submenus.Add(menuItem);
+                }
+
+                foreach (var dir in directory.GetDirectories())
+                {
+                    var menuItem = new FolderMenu();
+                    menuItem.Title = "[D] " + dir.Name;
+                    menuItem.Source = dir.FullName;
+                    menuItem.SourceType = Model.SourceType.Folder;
 
                     Submenus.Add(menuItem);
                 }
